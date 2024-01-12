d399eb9d9d51feababfae172109c039207d8a26c
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-01-12 19:49:38 +0600

    Add 1.3: course information

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,7 +8,7 @@ const Header = (props) =>{
 
 const Part = (props) =>{
   return(
-    <p key={props.content[1]}>{props.content[0]}: {props.content[1]}</p>
+    <p key={props.content["name"]}>{props.content["name"]}: {props.content["exercises"]}</p>
   )
 }
 
@@ -32,18 +32,24 @@ const Total = (props) =>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content items={[[part1,exercises1],[part2,exercises2],[part3,exercises3]]} />
-      <Total num={exercises1 + exercises2 + exercises3}/>
+      <Content items={[part1,part2,part3]} />
+      <Total num={part1["exercises"] + part2["exercises"] + part3["exercises"]}/>
     </div>
   )
 }
