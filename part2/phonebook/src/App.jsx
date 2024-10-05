c7f14090cee5865b28dd112ce89ec32bfe83e222
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-05 19:38:52 +0500

    Add check if name is already in the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,61 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12312312' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [records, setRecords] = useState([
+    { name: "Arto Hellas", number: "" },
+  ]);
+  const [name, setNewName] = useState("");
+  const [number, setNewNumber] = useState("");
 
-  const addNumber = (event) =>{
-    event.preventDefault()
-   const inputNumber = {
-      name:newName, number: newNumber
-    }
-   setPersons(persons.concat(inputNumber))
-
-    setNewNumber('')
-  }
-
-
-  const addName = (event) =>{
-    event.preventDefault()
-    const inputName = {
-      name: newName, number: ''
-    }
-    if (!persons.map(person =>person.name).includes(inputName.name))
-      setPersons(persons.concat(inputName))
-     else
-      window.alert(`${inputName.name} is already added to phonebook`)
-    
-    setNewName('')
-  }
+  const addRecord = (event) => {
+    event.preventDefault();
+    const newRecord = {
+      name: name,
+      number: number,
+    };
+    console.log(records);
+    if (records.some((record) => record.name === newRecord.name))
+      window.alert(`${newRecord.name} is already added to phonebook`);
+    else setRecords(records.concat(newRecord));
+    console.log(records);
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
+
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
-        
+      <form onSubmit={addRecord}>
         <div>
-          name: <input
-                value={newName}
-                onChange={handleNameChange}
-                />
+          name: <input value={name} onChange={handleNameChange} />
         </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div>
+          number: <input value={number} onChange={handleNumberChange} />
+        </div>
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-        {persons.map(entry => <div key={entry.name}>{entry.name}:{entry.number}</div>)}
+      {records.map((entry) => (
+        <div key={entry.name}>
+          {entry.name}:{entry.number}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
