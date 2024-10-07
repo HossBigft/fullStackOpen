1d1fd359a1e8cff6ca679d87d17d68cce6561e8e
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-07 22:40:51 +0500

    Extract axios functions to notes files

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,13 +9,20 @@ const App = () => {
   const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
+    noteService.getAll().then((response) => {
       setNotes(response.data);
     });
   }, []);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -22,7 +30,7 @@ const App = () => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     };
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+    noteService.create(noteObject).then((response) => {
       setNotes(notes.concat(response.data));
       setNewNote("");
     });
@@ -34,16 +42,6 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))
-    })
-  }
-  
   return (
     <div>
       <h1>Notes</h1>
